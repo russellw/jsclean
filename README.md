621178dbc520272e42a4fe19090bced3bcb34715
Russell Wallace <russell.wallace@gmail.com>
2015-05-25 18:53:43 +0100

    bin

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Reduces the entropy of JavaScript code:
 
 - Formats code in a standard layout, fixing readability of ill-formatted code and allowing a consistent style to be automatically enforced throughout a project.
 
-- Applies syntactic changes (add missing semicolons and optional braces, replace `==` with `===`, ensure array and object literals always have trailing commas and the final case of a `switch` always has a trailing `break` when no other terminator statement is present) that eliminate common sources of error and inconsistency.
+- Applies syntactic changes (add missing semicolons and optional braces, replace `==` with `===`, sort object properties, ensure array and object literals always have trailing commas and the final case of a `switch` always has a trailing `break` when no other terminator statement is present) that eliminate common sources of error and inconsistency.
 
 - Provides an API that allows parsing, syntactic transformation and formatted code generation to be accessed separately or together.
 
---
+++ bin/jsclean.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+'use strict';
+var commander = require('commander');
+var fs = require('fs');
+var stdin = require('get-stdin');
+
+	// options
+	commander.usage('[options] [files]');
+	commander.version(require('../package.json').version);
+	commander.option('    --no-exact-equals', 'don\'t replace == with ===');
+	commander.option('    --no-sort-properties', 'don\'t sort object properties');
+	commander.option('    --no-trailing-break', 'don\'t add trailing break to final case');
+	commander.option('-n, --no-backup', 'don\'t make .bak files');
+	commander.option('-s, --spaces <n>', 'indent with spaces', parseInt);
+	commander.parse(process.argv);
+	var options = defaults();
+	for (var p in commander) {
+		if (Object.prototype.hasOwnProperty.call(commander, p)) {
+			options[p] = commander[p];
+		}
+	}
+	if (commander.spaces) {
+		options.indent = '';
+		for (var i = commander.spaces; i-- > 0; ) {
+			options.indent += ' ';
+		}
+	}
+
+	// inputs
+	if (commander.args.length) {
+
+		// files
+		for (var file of commander.args) {
+			try {
+				var input = fs.readFileSync(file, {
+					encoding: 'utf8',
+				});
+			} catch (e) {
+				console.log(e.message);
+				process.exit(1);
+			}
+			try {
+				var output = format(input, options);
+			} catch (e) {
+				console.log(file + ': ' + e.message);
+				process.exit(1);
+			}
+			if (input === output) {
+				continue;
+			}
+			if (commander.backup) {
+				try {
+					fs.unlinkSync(file + '.bak');
+					fs.renameSync(file, file + '.bak');
+				} catch (e) {
+				}
+			}
+			try {
+				fs.writeFileSync(file, output);
+			} catch (e) {
+				console.log(e.message);
+				process.exit(1);
+			}
+			console.log(file);
+		}
+	} else {
+
+		// stdin
+		stdin(function (input) {
+			try {
+				var output = format(input, options);
+				console.log(output);
+			} catch (e) {
+				console.log(e.message);
+				process.exit(1);
+			}
+		});
+	}
